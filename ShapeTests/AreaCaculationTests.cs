cdbd2d33567ec4974fe2114856793a3ada09bfde
Alexey Yakimemko <aakimeno49@gmail.com>
2024-05-03 11:17:54 +0300

    Исправления

--- ShapeTests/AreaCaculationTests.cs
+++ ShapeTests/AreaCaculationTests.cs
@@ -1,6 +1,3 @@
-using ShapeLibrary;
-using ShapeLibrary.Shapes;
-
 namespace ShapeTests
 {
     public class AreaCaculationTests
--- ShapeTests/GlobalUsings.cs
+++ ShapeTests/GlobalUsings.cs
@@ -1,1 +1,3 @@
 global using NUnit.Framework;
+global using ShapeLibrary;
+global using ShapeLibrary.Shapes;
